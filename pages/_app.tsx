09272e1e71d30995c3baee4c2622d1c83e32b90e
Vincent <thisoneis4business@gmail.com>
2022-12-13 22:05:52 +1300

    fix: Black text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
         />
       </Head>
-      <div className="w-screen h-screen bg-white">
+      <div className="w-screen h-screen bg-white text-black">
         <Navigation />
         <Component {...pageProps} />
       </div>
