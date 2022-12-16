36dab4fc4f8a0bac42bf275e1a1de93d40721e76
Vincent <thisoneis4business@gmail.com>
2022-12-17 08:39:14 +1300

    feat: Fixed meta head links

--- components/Head.tsx
+++ components/Head.tsx
@@ -24,26 +24,26 @@ const Head: XFC<Props> = ({ title, description }) => {
       <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://dexclaimation.com/" />
-      <meta property="og:image" content="/me.png" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <meta property="og:image" content="https://dexclaimation.com/me.png" />
+      <link rel="icon" href="https://dexclaimation.com/favicon.ico" />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="https://dexclaimation.com/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="https://dexclaimation.com/favicon-16x16.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="https://dexclaimation.com/apple-touch-icon.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="https://dexclaimation.com/site.webmanifest" />
       <meta property="description" content={description ?? title} />
       <meta property="og:description" content={description ?? title} />
       <title>{title}</title>
