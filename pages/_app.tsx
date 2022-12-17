30c148c7baaca102844fca4c3c2795eb24208ccd
Vincent <thisoneis4business@gmail.com>
2022-12-17 14:03:02 +1300

    fix: Removed duplicate head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,14 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import type { FC } from "react";
 import Navigation from "../components/navigation/Navigation";
 import "../styles/globals.css";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
-        />
-      </Head>
-      <div className="w-screen h-screen bg-white text-black">
-        <Navigation />
-        <Component {...pageProps} />
-      </div>
-    </>
+    <div className="w-screen h-screen bg-white text-black">
+      <Navigation />
+      <Component {...pageProps} />
+    </div>
   );
 };
 
