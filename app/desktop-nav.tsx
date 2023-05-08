ab9027a61d76af31b252306656f234537555bfcc
Vincent <thisoneis4business@gmail.com>
2023-05-09 00:09:24 +1200

    feat: Optimise homepage with rsc

---
+++ app/desktop-nav.tsx
@@ -0,0 +1,78 @@
+//
+//  desktop-nav.tsx
+//  d-exclaimation
+//
+//  Created by d-exclaimation on 08 May 2023
+//
+
+import { entries } from "@d-exclaimation/common";
+import Link from "next/link";
+import { manifest } from "~/common/manifest";
+import Scrambled from "~/components/Scrambled";
+import { rc } from "~/next/rc";
+
+const scramble = {
+  delay: 10_000,
+  speed: 40,
+};
+
+const elements = entries(manifest.stories).map(
+  ([title, { img, ...other }]) => ({
+    title,
+    img,
+    ...other,
+  })
+);
+
+const DesktopNavigation = rc(() => {
+  return (
+    <nav
+      id="desktop-nav"
+      className="hidden lg:flex flex-col gap-2 p-[clamp(1rem,5vw,6rem)]"
+    >
+      {elements.map(({ title, img, ...props }) => (
+        <div key={`desktop-nav-${title}`}>
+          <Link
+            className="font-sans text-[clamp(3rem,4vw,6rem)] font-light no-underline
+            text-center lg:text-start transition-all duration-300 text-black/25
+            hover:text-blue-100 hover:z-30 group peer relative"
+            {...props}
+          >
+            <div
+              className="border-y-2 border-y-transparent transition-all
+              group-hover:border-y-neutral-400 group-hover:bg-neutral-300/10
+              scale-95 group-hover:scale-100 z-30 py-2"
+            >
+              <Scrambled
+                className="block relative opacity-30
+                group-hover:data-[dud=true]:text-neutral-400
+                group-hover:opacity-100 group-hover:z-30"
+                delay={scramble.delay}
+                speed={scramble.speed}
+                color={{
+                  dud: "data-[dud=true]:text-black",
+                  normal: "text-black",
+                }}
+                align="items-start"
+                justify="justify-start"
+                phrases={[title, title]}
+              />
+            </div>
+          </Link>
+          {img && (
+            <img
+              className="fixed -translate-y-1/2 -translate-x-1/2 top-1/2 left-[60%]
+              z-20 transition-all pointer-events-none max-w-[min(800px,80vw)] duration-300 
+              delay-200 peer-hover:delay-[0s] object-cover aspect-auto max-h-[min(600px,80vh)]
+              opacity-0 scale-50 peer-hover:opacity-100 peer-hover:scale-100"
+              alt={title}
+              src={img}
+            />
+          )}
+        </div>
+      ))}
+    </nav>
+  );
+});
+
+export default DesktopNavigation;
