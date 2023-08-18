4ab79e56a63a52afb9e8e3a26b7fd7f11e67cc7b
Vincent <thisoneis4business@gmail.com>
2023-08-18 17:51:39 +1200

    fix: fixed app router pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,41 @@
+const path = require("path");
+
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: ["app-build-manifest.json"],
 });
+
+const generateAppDirEntry = (entry) => {
+  const packagePath = require.resolve("next-pwa");
+  const packageDirectory = path.dirname(packagePath);
+  const registerJs = path.join(packageDirectory, "register.js");
+
+  return entry().then((entries) => {
+    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+      if (Array.isArray(entries["main-app"])) {
+        entries["main-app"].unshift(registerJs);
+      } else if (typeof entries["main-app"] === "string") {
+        entries["main-app"] = [registerJs, entries["main-app"]];
+      }
+    }
+    return entries;
+  });
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    const entry = generateAppDirEntry(config.entry);
+    config.entry = () => entry;
+
+    return config;
+  },
 };
 
 module.exports = withPWA(nextConfig);
